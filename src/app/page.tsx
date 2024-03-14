f0b6af6469e02ad16ef695f1fc47560e19f19102
Shohislom1112 <ravshanovshohislom11@gmail.com>
2024-03-14 15:56:56 +0500

    asd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,122 @@
-import Image from "next/image";
+import React from "react";
 
-export default function Home() {
+const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div className="container mx-auto h-min-screen">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+        reprehenderit cum, officia ut earum libero animi natus similique.
+        Sapiente facilis, reprehenderit fugiat suscipit, doloremque architecto
+        amet in iste, doloribus at cupiditate illum ipsam repudiandae ipsa
+        temporibus accusantium dolorum quis rerum debitis! Reiciendis provident
+        ratione ea totam id blanditiis, earum ab cumque ipsam incidunt
+        repellendus sint doloremque tempora, esse veniam iure rem perspiciatis
+        corrupti possimus nam et adipisci. Ullam architecto minus ut excepturi
+        quaerat id, ab fugit ipsam quod unde eius dolorum, qui voluptates nobis
+        sed perspiciatis laudantium doloribus dolores cupiditate eaque libero?
+        Harum minima vitae mollitia quos asperiores provident eligendi quibusdam
+        sunt ut placeat molestiae at, accusamus possimus neque, tempora quam
+        aperiam reprehenderit. Eum dolor eius, quibusdam ducimus velit est iure
+        eos culpa distinctio blanditiis impedit repudiandae dolorum rerum
+        deleniti nemo minus cumque quia recusandae amet suscipit nulla cum!
+        Voluptate beatae fugit omnis quibusdam asperiores! Dolorem, eaque
+        expedita tempora debitis illum, dignissimos dolore doloribus
+        voluptatibus maxime laudantium optio aspernatur culpa dicta tempore
+        possimus id accusamus fuga error nobis aliquam adipisci nam, vero quae!
+        Iure nostrum quam, accusamus iusto unde quaerat corrupti pariatur
+        obcaecati sunt debitis voluptatem, quas hic ipsum earum ab! Accusamus,
+        cum? Explicabo deleniti id modi quas voluptatem quaerat quis sed
+        laudantium, porro ab dolores et, dolor perferendis in! Rerum est
+        assumenda nulla numquam minima! Quas molestiae, mollitia alias error
+        fugiat architecto maiores repellendus esse, magnam omnis voluptatum at
+        voluptatibus corrupti accusamus nostrum rem. Tempore dolores, beatae vel
+        nam et exercitationem consequuntur velit! Est expedita recusandae
+        reprehenderit impedit excepturi magnam unde delectus laboriosam, porro
+        voluptatem sint reiciendis voluptatibus, officiis dolore. Architecto
+        obcaecati magni eveniet eius error iste recusandae, laborum laudantium
+        ullam laboriosam iure totam voluptas quam fugit nihil voluptates. Saepe
+        nisi adipisci quo, ratione, sapiente laboriosam voluptate libero
+        aspernatur sit, voluptatem officia eos. Mollitia eum illo qui quae,
+        corrupti omnis ad quas ipsa magnam placeat tempora provident
+        necessitatibus voluptatibus eaque magni molestiae quaerat sunt nobis
+        voluptas, quo quidem deleniti sed nemo. Quos doloribus quisquam tenetur
+        quod suscipit quasi veniam id voluptas pariatur. Natus velit, voluptate
+        voluptatem dolore libero inventore labore rerum dolorum earum optio
+        laudantium maiores quo aspernatur! Ab sint repellendus officia
+        reiciendis porro. Ex repudiandae facere, pariatur praesentium iusto
+        accusantium nemo blanditiis doloribus quos? Illo accusantium repudiandae
+        excepturi nihil in eius consectetur sunt quis. Ut nam reiciendis
+        incidunt sed dignissimos voluptatem perspiciatis optio sapiente odit
+        fuga nisi cupiditate eos beatae, amet nobis earum rerum sit molestiae
+        laboriosam alias eius soluta. Ea nihil eius necessitatibus, blanditiis
+        praesentium veritatis quidem illo voluptatem eum minus commodi
+        doloremque a sit asperiores accusamus laboriosam possimus quam suscipit
+        rem aut cum vel saepe. Aliquid ipsa porro aspernatur quos aut
+        necessitatibus odit saepe, quidem reiciendis facere neque inventore,
+        aliquam, eaque nisi tempore fugiat doloremque corrupti? Perferendis
+        reprehenderit cum eveniet consequatur dolore eos. Vel quidem ab dolore
+        vero aliquam fugiat ea veritatis deserunt obcaecati tenetur ipsam
+        repellendus, aliquid maiores error suscipit. Optio veniam, aut corporis
+        cupiditate at id vitae, aliquam non ex sed, inventore amet suscipit.
+        Dolores, ducimus consequuntur quidem libero voluptates praesentium,
+        aliquam dolor, sapiente totam quisquam nostrum delectus ex molestiae
+        maxime repellat quos. Impedit esse magnam, nesciunt, aliquam fugiat
+        sapiente illum, qui ratione odit itaque libero neque distinctio delectus
+        consequatur nulla dignissimos. Suscipit repellendus placeat cupiditate
+        fugiat a officiis sed optio laborum ipsa, enim neque ducimus vel nulla
+        officia? Ex ipsam autem voluptatibus ducimus est aperiam assumenda
+        veritatis accusantium quaerat illo tempore quos, cumque provident. A
+        dicta voluptate fugiat nihil. Vel inventore commodi blanditiis esse
+        exercitationem fuga sapiente hic possimus quisquam, at reprehenderit
+        cupiditate ut, dicta labore aperiam quibusdam similique! Delectus
+        debitis quidem molestias libero rem pariatur ipsam labore saepe, ea hic
+        consequatur ut atque eos beatae porro quam totam at? Officia inventore
+        repellat odio corrupti labore, tempora aut corporis pariatur et,
+        exercitationem eos ipsa nihil quia facilis voluptate temporibus. Rerum
+        dolore aspernatur quidem doloremque. Eligendi, totam explicabo modi ut
+        optio aut consequuntur rerum quis debitis maxime nulla? Reiciendis
+        pariatur quibusdam tempora, rerum eligendi maiores neque temporibus
+        ratione. Sint ullam, aliquam quaerat, harum debitis et dolor eaque
+        accusantium, velit aut expedita consectetur veritatis. Impedit odio,
+        sint asperiores cupiditate tempora architecto, facere ducimus est quos
+        labore nam eum. Omnis atque facilis amet nulla vel! Natus atque numquam
+        commodi, iste veritatis accusantium autem aliquam cupiditate provident
+        corrupti ut dolorum voluptatibus ad magni, dicta ab nobis aperiam,
+        perspiciatis iure cum id. Ipsum unde rerum eaque? Doloremque suscipit
+        dolorum vitae facilis deleniti exercitationem molestias nostrum dolore
+        pariatur ex cumque consequuntur molestiae nesciunt itaque, eveniet
+        ratione ea odit explicabo eum. Sint, quae magni. Fuga, mollitia. Animi
+        est dolorem nesciunt illo perferendis repellat porro, velit sit dicta
+        dolore sint perspiciatis quis tenetur consequuntur amet incidunt
+        adipisci saepe! Quaerat, laudantium fuga odit unde magnam distinctio
+        cupiditate voluptates deleniti veniam minima tempora id? Repellat
+        doloremque eius quos, beatae, magni placeat impedit cumque odit earum
+        praesentium quia maiores inventore voluptatum expedita numquam at sequi?
+        Vel, enim. Animi nihil eum eos odio modi nulla, debitis eligendi ea
+        tenetur reprehenderit unde voluptate sapiente libero, quod quos error,
+        veniam impedit facere quas ratione magnam! Voluptas beatae cumque hic,
+        quaerat libero odio illo quibusdam aliquam vero! Vitae fugit aliquam
+        temporibus reiciendis incidunt nulla consequuntur quis totam laborum,
+        mollitia neque eius maiores eveniet nostrum esse necessitatibus eos aut.
+        Animi explicabo architecto, sapiente voluptatum quos dignissimos.
+        Cupiditate recusandae deleniti at non, quae ipsam et dolor praesentium
+        rem beatae possimus quam quasi vitae itaque molestiae accusantium
+        repudiandae inventore consequatur iure nulla impedit? Explicabo culpa
+        exercitationem dolores autem? Sequi modi cum totam, reprehenderit magni
+        fugit at qui inventore voluptates laudantium autem. At tempore sapiente
+        itaque asperiores animi molestias doloribus sunt rerum ullam! Quo neque,
+        earum minus delectus quia rem rerum repellendus, totam, modi enim
+        officiis corrupti laudantium aliquam necessitatibus nobis facere.
+        Laborum officia ipsum ad animi, totam repudiandae soluta deleniti odio
+        pariatur unde harum, eveniet laudantium accusamus sunt! Obcaecati a
+        quisquam maxime deserunt accusamus. Nulla ipsa illum voluptate facilis
+        natus accusamus modi eius itaque tempora quod incidunt sequi similique
+        quis, molestias vero pariatur eveniet dolores ducimus officiis. Deleniti
+        quibusdam, fugiat magni molestias quasi, asperiores tempore modi
+        necessitatibus libero voluptatum, placeat quisquam.
+      </p>
+    </div>
   );
-}
+};
+
+export default Home;
---
+++ src/components/Header.tsx
@@ -0,0 +1,166 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { IoIosSearch } from "react-icons/io";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { FaRegUserCircle } from "react-icons/fa";
+import Link from "next/link";
+import ThemeSwitch from "./ThemeSwitch";
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isShrunk, setIsShrunk] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsShrunk(true);
+      } else {
+        setIsShrunk(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div
+      className={`bg-white dark:bg-[#1a253c] -top-1 left-0 right-0 z-20 fixed ${
+        isShrunk ? "shadow-lg" : ""
+      }`}
+    >
+      <div
+        className={`container mx-auto ${
+          isShrunk
+            ? "h-[85px] flex flex-col py-[6px] gap-y-1 duration-300"
+            : "h-[132px] flex flex-col py-4 gap-y-4 duration-300  "
+        }  px-5`}
+      >
+        <div className="flex justify-between items-center">
+          <IoIosSearch className="w-[24px] h-[24px] cursor-pointer hidden sm:block dark:text-slate-200 text-[#22202E] hover:text-slate-950" />
+          <p className="text-3xl font-normal text-[#101727] dark:text-white">
+            Avion
+          </p>
+          <div className="flex gap-4 jus items-center">
+            <IoIosSearch className="block md:hidden  w-[28px] h-[28px] cursor-pointer dark:text-slate-200 text-[#22202E] hover:text-slate-950" />
+            <AiOutlineShoppingCart className="md:block hidden  w-[24px] h-[24px] cursor-pointer dark:text-slate-200 text-[#22202E] hover:text-slate-950" />
+            <FaRegUserCircle className="md:block hidden w-[24px] h-[24px] cursor-pointer dark:text-slate-200 text-[#22202E] hover:text-slate-950" />
+            <ThemeSwitch />
+            <button
+              onClick={handleClick}
+              className="pt-2 flex-col gap-1 justify-center items-center block md:hidden w-[32px] h-[32px] cursor-pointer dark:text-slate-200 text-[#22202E] hover:text-slate-950"
+            >
+              <span
+                className={`bg-steel-500 block transition-all duration-300 ease-out
+            h-1 w-8 rounded-md bg-black dark:bg-slate-200 ${
+              isOpen
+                ? "rotate-45 translate-y-[0.41rem]"
+                : "-translate-y-[0.30rem]"
+            }`}
+              ></span>
+              <span
+                className={`bg-steel-500 block
+            h-1 w-8 rounded-md my-0.5 bg-black dark:bg-slate-200 ${
+              isOpen ? "opacity-0" : "opacity-100"
+            }`}
+              ></span>
+              <span
+                className={`bg-steel-500 block transition-all duration-300 ease-out
+            h-1 w-8 rounded-md bg-black dark:bg-slate-200  ${
+              isOpen
+                ? "-rotate-45 -translate-y-[0.35rem]"
+                : "translate-y-[0.30rem]"
+            }`}
+              ></span>
+            </button>
+          </div>
+        </div>
+        <hr className=" md:block hidden pr-6 h-2" />
+        <div className=" justify-center gap-6 hidden md:flex">
+          <Link
+            href={"#"}
+            className="text-[#505977] dark:text-slate-400 dark:hover:text-slate-200 hover:text-[#110f16] cursor-pointer hover:font-normal"
+          >
+            Plant pots
+          </Link>
+          <Link
+            href={"#"}
+            className="text-[#505977] dark:text-slate-400 dark:hover:text-slate-200 hover:text-[#110f16]  cursor-pointer hover:font-normal"
+          >
+            Ceramics
+          </Link>
+          <Link
+            href={"#"}
+            className="text-[#505977] dark:text-slate-400 dark:hover:text-slate-200 hover:text-[#110f16]  cursor-pointer hover:font-normal"
+          >
+            Tables
+          </Link>
+          <Link
+            href={"#"}
+            className="text-[#505977] dark:text-slate-400 dark:hover:text-slate-200 hover:text-[#110f16]  cursor-pointer hover:font-normal"
+          >
+            Chairs
+          </Link>
+          <Link
+            href={"#"}
+            className="text-[#505977] dark:text-slate-400 dark:hover:text-slate-200 hover:text-[#110f16]  cursor-pointer hover:font-normal"
+          >
+            Crockery
+          </Link>
+          <Link
+            href={"#"}
+            className="text-[#505977] dark:text-slate-400 dark:hover:text-slate-200 hover:text-[#110f16]  cursor-pointer hover:font-normal"
+          >
+            Tableware
+          </Link>
+          <Link
+            href={"#"}
+            className="text-[#505977] dark:text-slate-400 dark:hover:text-slate-200 hover:text-[#110f16]  cursor-pointer hover:font-normal"
+          >
+            Cutlery
+          </Link>
+        </div>
+        <div
+          className={`${
+            isOpen
+              ? "translate-x"
+              : "-translate-x-[420px] transation duration-400 ease-linier  "
+          } absolute top-0 left-0 h-screen w-[50vw] bg-slate-200 dark:bg-[#162037] z-50 transation duration-200 ease-linear md:hidden shadow-md`}
+        >
+          <div className="flex flex-col gap-4 pl-8 pt-4">
+            <p className="text-3xl font-normal text-[#101727] dark:text-white">
+              Avion
+            </p>
+            <Link
+              href={"#"}
+              className="text-[#505977] hover:text-[#110f16] cursor-pointer pt-4 hover:font-normal"
+            >
+              About us
+            </Link>
+            <Link
+              href={"#"}
+              className="text-[#505977] hover:text-[#110f16]  cursor-pointer hover:font-normal"
+            >
+              Contact
+            </Link>
+            <Link
+              href={"#"}
+              className="text-[#505977] hover:text-[#110f16]  cursor-pointer hover:font-normal"
+            >
+              Blog
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
